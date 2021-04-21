6b690af57d4c219c7a709fefb3cb5278588d374b
farayaaa <36233276+farayaaa@users.noreply.github.com>
2021-04-21 21:19:09 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './app';
+const express = require("express");
+const cors = require("cors");
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+const db = require("./db");
+
+const app = express();
+const port = 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(express.json());
+
+db.on('error', console.error.bind(console, 'MongoDB connection error: '));
+
+app.get('/', (req, res) => {
+    res.send('Hello, World!')
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+})
